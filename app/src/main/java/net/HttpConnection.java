8252a5a18f208b69c5660bd45fb33c6c7087aec9
350624690@qq.com <zx890117>
2016-10-21 10:40:01 +0800

    优化结构

--- app/src/main/java/net/HttpConnection.java
+++ app/src/main/java/net/HttpConnection.java
@@ -14,34 +14,36 @@ import Tool.LogUtil;
 public class HttpConnection {
     public static void sendRequest(final String address, final CallBackListener listener) {
         LogUtil.d("HttpConnection: 建立链接");
-                HttpURLConnection connection = null;
-                try {
-                    URL url = new URL(address);
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    connection.setDoInput(true);
-                    connection.connect();
-                    InputStream inputStream = connection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
-                    StringBuilder response = new StringBuilder();
-                    String line = null;
+        {
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(address);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(8000);
+                connection.setReadTimeout(8000);
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                StringBuilder response = new StringBuilder();
+                String line = null;
 
-                    while ((line = bufferedReader.readLine()) != null) {
-                        response.append(line);
-                    }
-
-                    if (listener != null) {
-                        listener.onComplete(response.toString());
-                    }
-                    bufferedReader.close();
-                    inputStream.close();
-                } catch (Exception e) {
-                    listener.onError(e);
-                } finally {
-                    if (connection != null)
-                        connection.disconnect();
+                while ((line = bufferedReader.readLine()) != null) {
+                    response.append(line);
                 }
+
+                if (listener != null) {
+                    listener.onComplete(response.toString());
+                }
+                bufferedReader.close();
+                inputStream.close();
+            } catch (Exception e) {
+                listener.onError(e);
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+            }
+        }
     }
 }
